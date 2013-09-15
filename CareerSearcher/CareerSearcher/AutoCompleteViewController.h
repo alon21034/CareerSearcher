74dab1155355dc81d44f36544fc7bb82aa7f7722
MD631 <md631@MD631teki-iMac.local>
2013-09-15 21:13:23 +0800

    add AutoCompleteViewController

---
+++ CareerSearcher/CareerSearcher/AutoCompleteViewController.h
@@ -0,0 +1,16 @@
+//
+//  AutoCompleteViewController.h
+//  CareerSearcher
+//
+//  Created by MD631 on 13/9/15.
+//  Copyright (c) 2013年 MD631. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AutoCompleteViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UITextField *mTextField;
+@property (strong, nonatomic) IBOutlet UITableView *mTableView;
+
+@end
---
+++ CareerSearcher/CareerSearcher/AutoCompleteViewController.m
@@ -0,0 +1,38 @@
+//
+//  AutoCompleteViewController.m
+//  CareerSearcher
+//
+//  Created by MD631 on 13/9/15.
+//  Copyright (c) 2013年 MD631. All rights reserved.
+//
+
+#import "AutoCompleteViewController.h"
+
+@interface AutoCompleteViewController ()
+
+@end
+
+@implementation AutoCompleteViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
