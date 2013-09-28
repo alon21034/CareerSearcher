ae890bd0de51533762c61432ac14a24dc1a0e0ca
MD631 <md631@MD631teki-iMac.local>
2013-09-28 16:05:23 +0800

    create new prom

---
+++ CareerSearcher/CareerSearcher/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CareerSearcher
+//
+//  Created by MD631 on 13/9/28.
+//  Copyright (c) 2013年 MD631. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
